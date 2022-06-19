8a937d9f4b7e79450dd6e240b6b20f877ccaf2d1
Shimanto Rehman <33893791+shimanto-rehman@users.noreply.github.com>
2022-06-20 02:14:25 +0600

    Home Typing Text and Birthday count down issue solved

--- js/babel.js
+++ js/babel.js
@@ -2,375 +2,381 @@
 /*-- -------------------------------- Birthday Counter Js Codes(Babel Js Code) -------------------------------------*/
 
 function countdown(endDate) {
-    let days, hours, minutes, seconds;
+  let days, hours, minutes, seconds;
 
-    endDate = new Date(endDate).getTime();
+  endDate = new Date(endDate).getTime();
 
-    if (isNaN(endDate)) {
-      return;
-    }
-
-    setInterval(calculate, 1000);
-
-    function calculate() {
-      let startDate = new Date();
-      startDate = startDate.getTime();
-
-      let timeRemaining = parseInt((endDate - startDate) / 1000);
-
-      if (timeRemaining >= 0) {
-        days = parseInt(timeRemaining / 86400);
-        timeRemaining = (timeRemaining % 86400);
-
-        hours = parseInt(timeRemaining / 3600);
-        timeRemaining = (timeRemaining % 3600);
-
-        minutes = parseInt(timeRemaining / 60);
-        timeRemaining = (timeRemaining % 60);
-
-        seconds = parseInt(timeRemaining);
-
-        document.getElementById("js-days").innerHTML = parseInt(days, 10);
-        document.getElementById("js-hours").innerHTML = ("0" + hours).slice(-2);
-        document.getElementById("js-minutes").innerHTML = ("0" + minutes).slice(-2);
-        document.getElementById("js-seconds").innerHTML = ("0" + seconds).slice(-2);
-      } else {
-        return;
-      }
-    }
+  if (isNaN(endDate)) {
+    return;
   }
 
-  (function () {
+  setInterval(calculate, 1000);
+
+  function calculate() {
+    let startDate = new Date();
+    startDate = startDate.getTime();
+
+    let timeRemaining = parseInt((endDate - startDate) / 1000);
+
+    if (timeRemaining >= 0) {
+      days = parseInt(timeRemaining / 86400);
+      timeRemaining = (timeRemaining % 86400);
+
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = (timeRemaining % 3600);
+
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = (timeRemaining % 60);
+
+      seconds = parseInt(timeRemaining);
+
+      document.getElementById("js-days").innerHTML = parseInt(days, 10);
+      document.getElementById("js-hours").innerHTML = ("0" + hours).slice(-2);
+      document.getElementById("js-minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("js-seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
+      return;
+    }
+  }
+}
+
+(function () {
+  let test = (new Date().getDate()) + (new Date().getMonth() * 30);
+  if (test < 168) {
     countdown((new Date().getFullYear()) + '-06-17');
-  }());
+  }
+  else {
+    countdown((new Date().getFullYear() + 1) + '-06-17');
+  }
+}());
 
 
-  /*----------------------------------------------- About Info Animation -------------------------------------------*/
+/*----------------------------------------------- About Info Animation -------------------------------------------*/
 
-  const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
-  };
-  // The strings to morph between. You can change these to anything you want!
-  const texts = [
-    "Know",
-    "About",
-    "Me",
-    "&",
-    "What",
-    "I",
-    "do..."
-  ];
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2")
+};
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+  "Know",
+  "About",
+  "Me",
+  "&",
+  "What",
+  "I",
+  "do..."
+];
 
-  // Controls the speed of morphing.
-  const morphTime = 1;
-  const cooldownTime = 0.25;
+// Controls the speed of morphing.
+const morphTime = 1;
+const cooldownTime = 0.25;
 
-  let textIndex = texts.length - 1;
-  let time = new Date();
-  let morph = 0;
-  let cooldown = cooldownTime;
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+  // fraction = Math.cos(fraction * Math.PI) / -2 + .5;	
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
   elts.text1.textContent = texts[textIndex % texts.length];
   elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
 
-  function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
+function doCooldown() {
+  morph = 0;
 
-    let fraction = morph / morphTime;
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
 
-    if (fraction > 1) {
-      cooldown = cooldownTime;
-      fraction = 1;
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
     }
 
-    setMorph(fraction);
+    doMorph();
+  } else {
+    doCooldown();
   }
-
-  // A lot of the magic happens here, this is what applies the blur filter to the text.
-  function setMorph(fraction) {
-    // fraction = Math.cos(fraction * Math.PI) / -2 + .5;	
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-  }
-
-  function doCooldown() {
-    morph = 0;
-
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
-
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-  }
-
-  // Animation loop, which is called every frame.
-  function animate() {
-    requestAnimationFrame(animate);
-
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
-
-    cooldown -= dt;
-
-    if (cooldown <= 0) {
-      if (shouldIncrementIndex) {
-        textIndex++;
-      }
-
-      doMorph();
-    } else {
-      doCooldown();
-    }
-  }
-  // Start the animation.
-  animate();
+}
+// Start the animation.
+animate();
 
 /*---------------------------------------------- Crowd Simulation ------------------------------------------------*/
-  const config = {
-    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/open-peeps-sheet.png',
-    rows: 15,
-    cols: 7
+const config = {
+  src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/open-peeps-sheet.png',
+  rows: 15,
+  cols: 7
+}
+
+// UTILS
+
+const randomRange = (min, max) => min + Math.random() * (max - min)
+
+const randomIndex = (array) => randomRange(0, array.length) | 0
+
+const removeFromArray = (array, i) => array.splice(i, 1)[0]
+
+const removeItemFromArray = (array, item) => removeFromArray(array, array.indexOf(item))
+
+const removeRandomFromArray = (array) => removeFromArray(array, randomIndex(array))
+
+const getRandomFromArray = (array) => (
+  array[randomIndex(array) | 0]
+)
+
+// TWEEN FACTORIES
+
+const resetPeep = ({ stage, peep }) => {
+  const direction = Math.random() > 0.5 ? 1 : -1
+  // using an ease function to skew random to lower values to help hide that peeps have no legs
+  const offsetY = 100 - 250 * gsap.parseEase('power2.in')(Math.random())
+  const startY = window.innerHeight - peep.height + offsetY
+  let startX
+  let endX
+
+  if (direction === 1) {
+    startX = -peep.height
+    endX = window.innerHeight + 800
+    peep.scaleX = 1
+  } else {
+    startX = window.innerHeight + peep.width + 800
+    endX = 0
+    peep.scaleX = -1
   }
 
-  // UTILS
+  peep.x = startX
+  peep.y = startY
+  peep.anchorY = startY
 
-  const randomRange = (min, max) => min + Math.random() * (max - min)
+  return {
+    startX,
+    startY,
+    endX
+  }
+}
 
-  const randomIndex = (array) => randomRange(0, array.length) | 0
+const normalWalk = ({ peep, props }) => {
+  const {
+    startX,
+    startY,
+    endX
+  } = props
 
-  const removeFromArray = (array, i) => array.splice(i, 1)[0]
+  const xDuration = 10
+  const yDuration = 0.25
 
-  const removeItemFromArray = (array, item) => removeFromArray(array, array.indexOf(item))
+  const tl = gsap.timeline()
+  tl.timeScale(randomRange(0.5, 1.5))
+  tl.to(peep, {
+    duration: xDuration,
+    x: endX,
+    ease: 'none'
+  }, 0)
+  tl.to(peep, {
+    duration: yDuration,
+    repeat: xDuration / yDuration,
+    yoyo: true,
+    y: startY - 10
+  }, 0)
 
-  const removeRandomFromArray = (array) => removeFromArray(array, randomIndex(array))
+  return tl
+}
 
-  const getRandomFromArray = (array) => (
-    array[randomIndex(array) | 0]
-  )
+const walks = [
+  normalWalk,
+]
 
-  // TWEEN FACTORIES
+// CLASSES
 
-  const resetPeep = ({ stage, peep }) => {
-    const direction = Math.random() > 0.5 ? 1 : -1
-    // using an ease function to skew random to lower values to help hide that peeps have no legs
-    const offsetY = 100 - 250 * gsap.parseEase('power2.in')(Math.random())
-    const startY = window.innerHeight - peep.height + offsetY
-    let startX
-    let endX
+class Peep {
+  constructor({
+    image,
+    rect,
+  }) {
+    this.image = image
+    this.setRect(rect)
 
-    if (direction === 1) {
-      startX = -peep.height
-      endX = window.innerHeight + 800
-      peep.scaleX = 1
-    } else {
-      startX = window.innerHeight + peep.width + 800
-      endX = 0
-      peep.scaleX = -1
-    }
-
-    peep.x = startX
-    peep.y = startY
-    peep.anchorY = startY
-
-    return {
-      startX,
-      startY,
-      endX
-    }
+    this.x = 0
+    this.y = 0
+    this.anchorY = 0
+    this.scaleX = 1
+    this.walk = null
   }
 
-  const normalWalk = ({ peep, props }) => {
-    const {
-      startX,
-      startY,
-      endX
-    } = props
+  setRect(rect) {
+    this.rect = rect
+    this.width = rect[2]
+    this.height = rect[3]
 
-    const xDuration = 10
-    const yDuration = 0.25
-
-    const tl = gsap.timeline()
-    tl.timeScale(randomRange(0.5, 1.5))
-    tl.to(peep, {
-      duration: xDuration,
-      x: endX,
-      ease: 'none'
-    }, 0)
-    tl.to(peep, {
-      duration: yDuration,
-      repeat: xDuration / yDuration,
-      yoyo: true,
-      y: startY - 10
-    }, 0)
-
-    return tl
+    this.drawArgs = [
+      this.image,
+      ...rect,
+      0, 0, this.width, this.height
+    ]
   }
 
-  const walks = [
-    normalWalk,
-  ]
-
-  // CLASSES
-
-  class Peep {
-    constructor({
-      image,
-      rect,
-    }) {
-      this.image = image
-      this.setRect(rect)
-
-      this.x = 0
-      this.y = 0
-      this.anchorY = 0
-      this.scaleX = 1
-      this.walk = null
-    }
-
-    setRect(rect) {
-      this.rect = rect
-      this.width = rect[2]
-      this.height = rect[3]
-
-      this.drawArgs = [
-        this.image,
-        ...rect,
-        0, 0, this.width, this.height
-      ]
-    }
-
-    render(ctx) {
-      ctx.save()
-      ctx.translate(this.x, this.y)
-      ctx.scale(this.scaleX, 1)
-      ctx.drawImage(...this.drawArgs)
-      ctx.restore()
-    }
-  }
-
-  // MAIN
-
-  const img = document.createElement('img')
-  img.onload = init
-  img.src = config.src
-
-  const canvas = document.querySelector('#crowd-simulator')
-  const ctx = canvas.getContext('2d')
-
-  const stage = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  }
-
-  const allPeeps = []
-  const availablePeeps = []
-  const crowd = []
-
-  function init() {
-    createPeeps()
-
-    // resize also (re)populates the stage
-    resize()
-
-    gsap.ticker.add(render)
-    window.addEventListener('resize', resize)
-  }
-
-  function createPeeps() {
-    const {
-      rows,
-      cols
-    } = config
-    const {
-      naturalWidth: width,
-      naturalHeight: height
-    } = img
-    const total = rows * cols
-    const rectWidth = width / rows
-    const rectHeight = height / cols
-
-    for (let i = 0; i < total; i++) {
-      allPeeps.push(new Peep({
-        image: img,
-        rect: [
-          (i % rows) * rectWidth,
-          (i / rows | 0) * rectHeight,
-          rectWidth,
-          rectHeight,
-        ]
-      }))
-    }
-  }
-
-  function resize() {
-    stage.width = canvas.clientWidth
-    stage.height = canvas.clientHeight
-    canvas.width = window.innerWidth * devicePixelRatio
-    canvas.height = window.innerHeight * devicePixelRatio
-
-    crowd.forEach((peep) => {
-      peep.walk.kill()
-    })
-
-    crowd.length = 0
-    availablePeeps.length = 0
-    availablePeeps.push(...allPeeps)
-
-    initCrowd()
-  }
-
-  function initCrowd() {
-    while (availablePeeps.length) {
-      // setting random tween progress spreads the peeps out
-      addPeepToCrowd().walk.progress(Math.random())
-    }
-  }
-
-  function addPeepToCrowd() {
-    const peep = removeRandomFromArray(availablePeeps)
-    const walk = getRandomFromArray(walks)({
-      peep,
-      props: resetPeep({
-        peep,
-        stage,
-      })
-    }).eventCallback('onComplete', () => {
-      removePeepFromCrowd(peep)
-      addPeepToCrowd()
-    })
-
-    peep.walk = walk
-
-    crowd.push(peep)
-    crowd.sort((a, b) => a.anchorY - b.anchorY)
-
-    return peep
-  }
-
-  function removePeepFromCrowd(peep) {
-    removeItemFromArray(crowd, peep)
-    availablePeeps.push(peep)
-  }
-
-  function render() {
-    canvas.width = canvas.width
+  render(ctx) {
     ctx.save()
-    ctx.scale(devicePixelRatio, devicePixelRatio)
-
-    crowd.forEach((peep) => {
-      peep.render(ctx)
-    })
-
+    ctx.translate(this.x, this.y)
+    ctx.scale(this.scaleX, 1)
+    ctx.drawImage(...this.drawArgs)
     ctx.restore()
   }
+}
+
+// MAIN
+
+const img = document.createElement('img')
+img.onload = init
+img.src = config.src
+
+const canvas = document.querySelector('#crowd-simulator')
+const ctx = canvas.getContext('2d')
+
+const stage = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const allPeeps = []
+const availablePeeps = []
+const crowd = []
+
+function init() {
+  createPeeps()
+
+  // resize also (re)populates the stage
+  resize()
+
+  gsap.ticker.add(render)
+  window.addEventListener('resize', resize)
+}
+
+function createPeeps() {
+  const {
+    rows,
+    cols
+  } = config
+  const {
+    naturalWidth: width,
+    naturalHeight: height
+  } = img
+  const total = rows * cols
+  const rectWidth = width / rows
+  const rectHeight = height / cols
+
+  for (let i = 0; i < total; i++) {
+    allPeeps.push(new Peep({
+      image: img,
+      rect: [
+        (i % rows) * rectWidth,
+        (i / rows | 0) * rectHeight,
+        rectWidth,
+        rectHeight,
+      ]
+    }))
+  }
+}
+
+function resize() {
+  stage.width = canvas.clientWidth
+  stage.height = canvas.clientHeight
+  canvas.width = window.innerWidth * devicePixelRatio
+  canvas.height = window.innerHeight * devicePixelRatio
+
+  crowd.forEach((peep) => {
+    peep.walk.kill()
+  })
+
+  crowd.length = 0
+  availablePeeps.length = 0
+  availablePeeps.push(...allPeeps)
+
+  initCrowd()
+}
+
+function initCrowd() {
+  while (availablePeeps.length) {
+    // setting random tween progress spreads the peeps out
+    addPeepToCrowd().walk.progress(Math.random())
+  }
+}
+
+function addPeepToCrowd() {
+  const peep = removeRandomFromArray(availablePeeps)
+  const walk = getRandomFromArray(walks)({
+    peep,
+    props: resetPeep({
+      peep,
+      stage,
+    })
+  }).eventCallback('onComplete', () => {
+    removePeepFromCrowd(peep)
+    addPeepToCrowd()
+  })
+
+  peep.walk = walk
+
+  crowd.push(peep)
+  crowd.sort((a, b) => a.anchorY - b.anchorY)
+
+  return peep
+}
+
+function removePeepFromCrowd(peep) {
+  removeItemFromArray(crowd, peep)
+  availablePeeps.push(peep)
+}
+
+function render() {
+  canvas.width = canvas.width
+  ctx.save()
+  ctx.scale(devicePixelRatio, devicePixelRatio)
+
+  crowd.forEach((peep) => {
+    peep.render(ctx)
+  })
+
+  ctx.restore()
+}
